f0b87ccbdb4c0cc76f34dc03d8bd47254e131597
AnaAmora <104854292+AnaAmora@users.noreply.github.com>
2024-04-19 09:57:08 -0300

    .

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-//omecaCronometro();
+comecaCronometro();
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -48,12 +48,14 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+
     segundos %= 60;
-minutos %= 60;
-horas %= 24;
-if (tempoFinal > 0){
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-} else {
-    return "Prazo Finalizado";
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos]
+    } else {
+        return [0,0,0,0];
+    }
 }
-}
+
